1af7b1d624fa01845801168ae9b9957d362c830f
MLamb14 <54187801+MLamb14@users.noreply.github.com>
2019-09-10 22:07:52 +0100

    Update main.js

--- band-project/scripts/main.js
+++ band-project/scripts/main.js
@@ -1,9 +1,9 @@
 var adjectives = ['Enticing', 'Confusing', 'Gyrating', 'Stolen', 'Forbidden'] // array of adjectives
 var nouns = ['Antelope', 'Wristwatch', 'Geranium', 'Clef', 'Moon', 'Mountain'] // array of nouns
 
-function randomWord(array) {
+function randomWord(array) { // this function is to avoid having to make vars with Math.random every time
     var word = array[Math.floor(Math.random() * array.length)];
-    return word;
+    return word; // return word makes it come back with a word/variable after running the function
 }
 
 function myGenerate() {
