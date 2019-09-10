04bcad396ec6497c39557c073467f8cea791a2a5
MLamb14 <54187801+MLamb14@users.noreply.github.com>
2019-09-10 11:37:25 +0100

    Update main.js
    Created a function to create random variables, removed the variables that use Math.random

--- band-project/scripts/main.js
+++ band-project/scripts/main.js
@@ -1,10 +1,14 @@
-// this is the start of the file
 var adjectives = ['Enticing', 'Confusing', 'Gyrating', 'Stolen', 'Forbidden'] // array of adjectives
 var nouns = ['Antelope', 'Wristwatch', 'Geranium', 'Clef', 'Moon', 'Mountain'] // array of nouns
 
+function randomWord(array) {
+    var word = array[Math.floor(Math.random() * array.length)];
+    return word;
+}
+
 function myGenerate() {
-    var rand1 = adjectives[Math.floor(Math.random() * adjectives.length)]; // this gives me a var which is a random one of var adjectives
-    var rand2 = nouns[Math.floor(Math.random() * nouns.length)]; // same as above
+    var rand1 = randomWord(adjectives);
+    var rand2 = randomWord(nouns);
     document.getElementById("word-one").innerHTML = rand1;
     document.getElementById("word-two").innerHTML = rand2;
 } // this function replaces Word One and Two with random ones from the array above
