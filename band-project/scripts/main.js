5568716b5fab525c7a08746cc63803e7185c4d09
MLamb14 <54187801+MLamb14@users.noreply.github.com>
2019-09-11 10:20:18 +0100

    Update main.js

--- band-project/scripts/main.js
+++ band-project/scripts/main.js
@@ -6,9 +6,20 @@ function randomWord(array) { // this function is to avoid having to make vars wi
     return word; // return word makes it come back with a word/variable after running the function
 }
 
+var checkBoxAdjective = document.getElementById("adj-only");
+var checkBoxNouns = document.getElementById("nouns-only");
+
 function myGenerate() {
-    var rand1 = randomWord(adjectives);
-    var rand2 = randomWord(nouns);
-    document.getElementById("word-one").innerHTML = rand1;
-    document.getElementById("word-two").innerHTML = rand2;
+    if (checkBoxAdjective.checked == true){
+    document.getElementById("word-one").innerHTML = randomWord(adjectives);
+    document.getElementById("word-two").innerHTML = randomWord(adjectives);
+    } else if (checkBoxNouns.checked == true){
+        document.getElementById("word-one").innerHTML = randomWord(nouns);
+        document.getElementById("word-two").innerHTML = randomWord(nouns);
+        }
+        else {
+            document.getElementById("word-one").innerHTML = randomWord(adjectives);
+            document.getElementById("word-two").innerHTML = randomWord(nouns);
+        }
 } // this function replaces Word One and Two with random ones from the array above
+// Added if and else so checkboxes can modify the results/arrays used
